c32c859fd464f0aa8361c0fdd6082c3375e1aa95
Kazuma Nishikawa <120302595+Kazuma-Nishikawa@users.noreply.github.com>
2023-01-26 13:42:29 +0900

    Add files via upload

--- frontA vue/app.js
+++ frontA vue/app.js
@@ -1,34 +1,28 @@
-new Vue ({
-    el: "#app",
-    data: {
-        message: "apple",
-        items: [
-            1,2,3
-        ],
-        color: '',
+ new Vue ({
+    el:"#app",
+    data:{
+       text : "Grape",
+    },
+    mounted() {
+        this.requestMessage()
     },
     methods: {
-        change: function(){
-            
-        }
-}
-})
-//  const btnElement = document.getElementById("btn");
-//         let message =  "Cats have about 130,000 hairs per square inch (20,155 hairs per square centimeter)."
-        
-//         const textElement = document.getElementById("name");
-//         textElement.textContent = message
-        
-//         const onClickUpdate = ()=> {
-//             let request = new XMLHttpRequest();
-//             request.open ('GET',"https://catfact.ninja/fact",true)
-//             request.responseType ="json"
-//             request.onload =()=> {
-//                 const data =request.response;
-//                 console.log(data.fact);
-//                 textElement.textContent = data.fact;
-
-//             }
-//             request.send();
-//         }
-//         btn.addEventListener('click',onClickUpdate)
+        onClickUpdate : function() {
+            this.requestMessage()
+        },
+        requestMessage: function(){
+            const request = new XMLHttpRequest()
+            request.open("GET","https://catfact.ninja/fact",true)
+            request.responseType ="json"
+            request.onload = ()=> {
+                this.text = request.response.fact
+                console.log(request.response.fact, request.status)
+            }
+            request.send()
+        },
+    },
+    computed: {
+       
+    }
+ })
+ 
